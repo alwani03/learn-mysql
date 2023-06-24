5705992f824a1e90e13a4a35aeaf625a90443bab
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-06-24 14:28:45 +0700

    Update Locking

--- sql/locking.sql
+++ sql/locking.sql
@@ -29,11 +29,11 @@ START TRANSACTION ;
 
 SELECT * FROm products;
 
-SELECT * FROM products WHERE id = 'P0001' FOR UPDATE;
+SELECT * FROM products WHERE id = 'P001' FOR UPDATE;
 
 UPDATE products
 SET quantity = quantity - 10
-WHERE id = 'P0001';
+WHERE id = 'P001';
 
 COMMIT ;
 
@@ -42,11 +42,11 @@ START TRANSACTION ;
 
 SELECT * FROm products;
 
-SELECT * FROM products WHERE id = 'P0001' FOR UPDATE ;
+SELECT * FROM products WHERE id = 'P001' FOR UPDATE ;
 
 UPDATE products
 SET quantity = quantity - 10
-WHERE id = 'P0001';
+WHERE id = 'P001';
 
 COMMIT ;
 
@@ -55,15 +55,50 @@ COMMIT ;
 
 START TRANSACTION ;
 
-SELECT * FROM products WHERE id = 'P002' FOR UPDATE ;
-
 SELECT * FROM products WHERE id = 'P001' FOR UPDATE ;
+
+SELECT * FROM products WHERE id = 'P002' FOR UPDATE ;
 
 
 #  DEADLOCK user 2
 
 START TRANSACTION ;
 
+SELECT * FROM products WHERE id = 'P002' FOR UPDATE ;
+
 SELECT * FROM products WHERE id = 'P001' FOR UPDATE ;
 
-SELECT * FROM products WHERE id = 'P002' FOR UPDATE ;
+/* Lock Table Read user 1 masih bisa*/
+LOCK TABLE products READ;
+
+UPDATE products
+SET quantity = quantity - 10
+WHERE id = 'P001';
+
+UNLOCK TABLE;
+
+
+/* Lock Table Write  hanya bisa write user 1 dan user 2 tidak bisa ngapa" in*/
+
+LOCK TABLE products WRITE;
+
+UPDATE products
+SET quantity = quantity - 10
+WHERE id = 'P001';
+
+UNLOCK TABLE;
+
+
+# LOCK INSTANCE
+
+LOCK INSTANCE FOR BACKUP;
+
+UNLOCK INSTANCE;
+
+# LOCK INSTANCE
+
+ALTER TABLE products
+ADD column sample VARCHAR(100);
+
+
+
