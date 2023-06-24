74c6335de4850f342cbf10c050b49b5aeb760de3
alwani achmad <alwaniachmad7031999aa@gmail.com>
2023-06-24 17:11:52 +0700

    Restore and backups update

--- sql/restore and backup.sql
+++ sql/restore and backup.sql
@@ -1,1 +1,8 @@
 https://devilbox.readthedocs.io/en/latest/maintenance/backup-and-restore-mysql.html
+
+--backup
+https://www.udemy.com/course/database-mysql-pemula-sampai-mahir/learn/lecture/24091050#overview
+
+
+--restore
+https://www.udemy.com/course/database-mysql-pemula-sampai-mahir/learn/lecture/24091052#overview
